c6cde857d95fd428a02a5c564b44da2024d4ddf6
kenlai93 <37834701+kenlai93@users.noreply.github.com>
2018-03-27 22:22:50 +0800

    Add files via upload

--- po.c
+++ po.c
@@ -9,12 +9,12 @@ int main(int argc, char *argv[]) {
 		printf("Error: the number of users is not in the range, 3 to 9.");
 		exit(0);
 	}
-	char user[80][userCount];
+	char *user[userCount];
 	printf("%d users\n", userCount);
 	int i;
 	for(i = 0; i < userCount; i++) {
-		argv[i+1][0]=toupper(argv[i+1][0]); // capitalize first letter
-		strcpy(user[i],argv[i+1]);
+		argv[i + 1][0] -= 32; // capitalize first letter
+		user[i] = argv[i + 1];
 		printf("%s added\n", user[i]);
 	}
 	
@@ -63,80 +63,4 @@ int main(int argc, char *argv[]) {
 		
 		else printf("wrong command!\n");
 	}
-	
-	
-	
-	
-	for (i=0;i<3;i++){
-		returnpid = fork();
-		if (returnpid < 0) {
-			printf("Fork failed\n");
-			exit(1);
-		}
-		else if (returnpid == 0) { /* child */
-			close(fd[idx][1]);
-			//printf("child %d waiting read n:%d\n",idx,read(fd[idx][0],buf,80));
-			while ((n = read(fd[idx][0],buf,80)) > 0) { 
-				printf("child %d successfully read\n",idx);
-			   buf[n] = 0;
-			   printf("<child %d> message [%s] of size %d bytes received\n",idx,buf,n);
-			   //break;
-			}
-			//printf("<child %d> %d,%d\n",idx,getpid(),getppid());
-			printf("<child %d> completed\n",idx);
-			close(fd[idx][0]);
-			exit(0);
-		}else if(i==?){
-			close(fd[idx][0]);
-			idx++;
-
-			aa=0;
-
-			sprintf(buf,"aaa");
-			sprintf(buf,"bbb");
-
-			//sleep(1);
-			//write(fd[0][1],"bbb",3);
-			//write(fd[1][1],"ccc",3);
-
-			write(fd[0][1],"aaa",3);
-			write(fd[1][1],"ddd",3);
-			write(fd[2][1],"ccc",3);
-			usleep(100);
-			write(fd[0][1],"eee",3);
-
-			/*
-			write(fd[1],"",0);
-			write(fd[1],"b",1);
-
-			write(fd[1],"b",1);
-			write(fd[1],"c",1);
-			write(fd[1],"d",1);
-			write(fd[1],"e",1);
-			*/
-			printf("<parent> I have finished:%d!\n",getpid());
-			//idx++; 
-		}
-		else{
-			close(fd[idx][0]);
-			idx++;
-		}
-	}
-  
-	for (i=0;i<3;i++){
-		wait(NULL);
-		close(fd[idx][1]);
-	}
-
-  
-	printf("<parent> completed\n");
-	exit(0);
-}
-
-	
-	
-	
-	
-	
-	
 }
